0f8e1b2c80bed3d0b1b4b40e57f94bdc7e299703
angie -u <lucidkodo@gmail.com>
2023-08-29 00:49:50 +0800

    Update production port number

--- buildable/vite.config.ts
+++ buildable/vite.config.ts
@@ -4,12 +4,14 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // local dev server
   server: {
     host: true,
     port: 4010,
   },
+  // production server
   preview: {
     host: true,
-    port: 4010,
+    port: 4011,
   },
 });
